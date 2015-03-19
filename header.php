4aa176f893c56ce086aa1c131b19a4b65aaa9a2f
jasmingimenez <jasmingimenez@gmail.com>
2015-03-18 23:49:53 -0600

    Create header.php
    template php header with navigation bar.

---
+++ header.php
@@ -0,0 +1,14 @@
+<!-- header -->
+<div class="header">
+<div class="header-top">
+Jasmin Gimenez
+</div>
+<div class="navigation">
+	<ul>
+    	<li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</div>
+</div>
+<!-- end header -->
